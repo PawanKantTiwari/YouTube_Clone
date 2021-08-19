f39b3ac116b0e699cfb5f91ed5ffc5503b667360
Pawan Kant Tiwari <p.k.t.7042@gmail.com>
2021-08-19 20:19:35 +0530

    API Problem Solved

--- src/apis/youtubeapi.js
+++ src/apis/youtubeapi.js
@@ -1,12 +1,12 @@
-import axios from "axios";
-const KEY = "AIzaSyCfbeP4qfwV96fPLY_lhklKVzXG-r13XLY";
 
+import axios from "axios";
+const KEY = "AIzaSyCS1swgcMiY4JZJieENxVv1KqCekOh25gE";
 
 export default axios.create({
-    baseURL : "https://www.googleapis.com/youtube/v3/",
-    params  : {
-        part: "snippet",
-        maxResult : 5,
-        key : KEY,
-    },
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+  params: {
+    part: "snippet",
+    maxResult: 5,
+    key: KEY,
+  },
 });
